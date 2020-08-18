7cb20866f27cfb3635aabac45679867bd996b316
cbarros7 <cj.barros07@gmail.com>
2020-08-17 19:38:48 -0500

    Update deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Script that distributes an archive to your web servers, 
+# Script that distributes an archive to your web servers,
 # using the function do_deploy
 """do deploy web static"""
 from fabric.api import *
@@ -26,53 +26,43 @@ def do_pack():
     else:
         return file_local
 
+
 def do_deploy(archive_path):
     """function do_deploy"""
-    path_file = archive_path.split('/')[-1]
-    folder = path_file.split('.')[0]
-    
-     # Upload directory
-    upload = put(archive_path, '/tmp/{}'.format(path_file))
+    try:
+        path_file = archive_path.split('/')[-1]
+        folder = path_file.split('.')[0]
 
-    if archive_path is None or upload.failed:
-        return False
-    
-    
-    if put(archive_path, "/tmp/{}".format(path_file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(folder)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(folder)).failed is True:
-        return False
-    
-    # run("rm -rf /data/web_static/releases/{}/".format(folder))
-    # Create folder
-    # run('mkdir -p /data/web_static/releases/{}/'.format(folder))
-    #if stat.failed:
-    #    return False
+        # Upload directory
+        if put(archive_path, "/tmp/{}".format(path_file)).failed is True:
+            return False
 
-    # Uncompress the archive
-    command = 'tar xvzf /tmp/{}'.format(path_file)
-    command += '-C /data/web_static/releases/{}'.format(folder)
-    run(command)
-    run('rm -rf /tmp/{}'.format(path_file))
-    command = 'mv /data/web_static/releases/{}/web_static/*'.format(folder)
-    command += '/data/web_static/releases/{}/'.format(folder)
-    run(command)
-    
-    clean = 'rm -rf /data/web_static/releases/{} /data/web_static/current'.format(folder)
-    run(clean)
-    
-    #Delete symbolic link
-    run('rm -rf /data/web_static/current')
-    # Create new symbolic link
-    run('ln -sfn /data/web_static/releases/{} /data/web_static/current'.format(folder))
+        if archive_path is None or upload.failed:
+            return False
 
-def deploy():
-    """Verify the status of deploy"""
-    file_name = do_pack()
-    if file_name is None:
+        run("rm -rf /data/web_static/releases/{}/".format(folder))
+        # Create folder
+        run('mkdir -p /data/web_static/releases/{}/'.format(folder))
+
+        # Uncompress the archive
+        command = 'tar xvzf /tmp/{}'.format(path_file)
+        command += '-C /data/web_static/releases/{}'.format(folder)
+        run(command)
+        run('rm -rf /tmp/{}'.format(path_file))
+        command = 'mv /data/web_static/releases/{}/web_static/*'.format(folder)
+        command += '/data/web_static/releases/{}/'.format(folder)
+        run(command)
+
+        clean = 'rm -rf /data/web_static/releases/{}\
+             /data/web_static/current'.format(folder)
+        run(clean)
+
+        # Delete symbolic link
+        run('rm -rf /data/web_static/current')
+        # Create new symbolic link
+        run('ln -sfn /data/web_static/releases/{}\
+            /data/web_static/current'.format(folder))
+        return True
+
+    except:
         return False
-    return do_deploy(file_name)
